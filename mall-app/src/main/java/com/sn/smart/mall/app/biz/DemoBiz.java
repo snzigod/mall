036b872289b4a7f7666b368e2635e77271c54c3b
snzigod <snzigod@hotmail.com>
2017-11-03 17:33:07 +0800

    初始化版本

--- mall-app/src/main/java/com/sn/smart/mall/app/biz/DemoBiz.java
+++ mall-app/src/main/java/com/sn/smart/mall/app/biz/DemoBiz.java
@@ -1,12 +1,18 @@
 package com.sn.smart.mall.app.biz;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.sn.smart.mall.api.DemoService;
 
 @Service
 public class DemoBiz {
+	
+	@Autowired
+	private DemoService demoService;
 
 	public String sayHello() {
-		return "hello, world!";
+		return demoService.sayHello();
 	}
 
 }
