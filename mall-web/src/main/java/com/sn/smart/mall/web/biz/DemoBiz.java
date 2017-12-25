23e62d6e14ce568e0309c440d9dce63d7bf38e74
snzigod <snzigod@hotmail.com>
2017-12-25 14:59:52 +0800

    初始化代码

--- mall-web/src/main/java/com/sn/smart/mall/web/biz/DemoBiz.java
+++ mall-web/src/main/java/com/sn/smart/mall/web/biz/DemoBiz.java
@@ -1,12 +1,18 @@
 package com.sn.smart.mall.web.biz;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.sn.smart.mall.api.DemoService;
 
 @Service
 public class DemoBiz {
 	
-	public String sayHello() {
-		return "Hello, world!";
+	@Autowired
+	private DemoService demoService;
+	
+	public String sayHello(String name) {
+		return demoService.sayHello(name);
 	}
 
 }
--- mall-web/src/main/java/com/sn/smart/mall/web/controller/DemoController.java
+++ mall-web/src/main/java/com/sn/smart/mall/web/controller/DemoController.java
@@ -7,18 +7,25 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sn.smart.mall.web.biz.DemoBiz;
+import com.sn.smart.mall.web.vo.DemoVo;
 
 @Controller
 @RequestMapping("/church")
 public class DemoController {
-	
+
 	@Autowired
 	private DemoBiz demoBiz;
-	
+
 	@ResponseBody
-	@RequestMapping(value = "/sayHello",method = RequestMethod.GET)
-	public String sayHello() {
-		return demoBiz.sayHello();
+	@RequestMapping(value = "/sayHello", method = RequestMethod.GET)
+	public String sayHello(String name) {
+		return demoBiz.sayHello(name);
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/getBeanParam", method = RequestMethod.GET)
+	public String getBeanParam(DemoVo demoVo) {
+		return demoBiz.sayHello(demoVo.getName());
 	}
 
 }
